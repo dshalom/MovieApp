345ff6a927cb87888bb899cc00250ebd5aca4027
David Shalom <david.shalom@sky.uk>
2023-12-31 16:02:41 +0000

    first commit

---
+++ app/src/main/java/com/ds/movieapp/App.kt
@@ -0,0 +1,19 @@
+package com.ds.movieapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    /** A tree which logs important information for crash reporting.  */
+}
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
+}
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.5.1")
+    }
+}
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+// to do add more dependencies here
