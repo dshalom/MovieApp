90903b2827a02ac2100999003587487d6f7cf46c
David Shalom <david.shalom@sky.uk>
2023-12-31 22:18:04 +0000

    added detekt

--- app/src/main/java/com/ds/movieapp/MainActivity.kt
+++ app/src/main/java/com/ds/movieapp/MainActivity.kt
@@ -19,7 +19,12 @@ class MainActivity : ComponentActivity() {
             MovieAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+
+
+                    modifier =
+
+
+                    Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
