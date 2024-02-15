196eb1f8e4b8d9671965eaa108a5a0a50314600b
David Shalom <david.shalom@sky.uk>
2024-02-15 16:45:00 +0000

    added details image (#14)

--- app/src/main/java/com/ds/movieapp/ui/screens/home/MoviesUi.kt
+++ app/src/main/java/com/ds/movieapp/ui/screens/home/MoviesUi.kt
@@ -1,13 +1,14 @@
 package com.ds.movieapp.ui.screens.home
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ds.movieapp.domain.models.Movie
@@ -20,10 +21,7 @@ fun MoviesUi(
     onMovieClicked: (String) -> Unit,
     onFavouriteClicked: (String, Boolean) -> Unit
 ) {
-    LazyRow(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), Modifier.height(LocalConfiguration.current.screenHeightDp.dp)) {
         items(movies) { movie ->
             Box(
                 modifier = Modifier.clickable {
