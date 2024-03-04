16d43ef2282d58780a843f893cd68c118a880448
David Shalom <david.shalom@sky.uk>
2024-03-04 17:32:40 +0000

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
